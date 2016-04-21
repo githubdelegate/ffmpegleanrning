fdcbed79d63349c01c032ae82a5f878304cbc49f
gitalpine <ostmail@163.com>
2016-04-21 20:24:39 +0800

    Initial Commit

---
+++ PCM_Function/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  PCM_Function
+//
+//  Created by zhangyun on 16/4/21.
+//  Copyright © 2016年 USC. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
